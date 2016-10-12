3d137d5bd6171e6355306abccc03b3fa1af0ccb8
Nico Kratky <nico@nicokratky.me>
2016-10-12 16:36:15 +0200

    moved code from libcreate.h into source file

---
+++ libcreate.c
@@ -0,0 +1,45 @@
+#include <kipr/botball.h>
+#include "include/libcreate.h"
+#include "include/create_codes.h"
+#include "include/create_velocities.h"
+
+
+void create_drives_direct(const short speed_l, const short speed_r) {
+
+    create_write_byte(OI_DRIVE_DIRECT);
+    create_write_byte(HIGH_BYTE(speed_l));
+    create_write_byte(LOW_BYTE(speed_l));
+    create_write_byte(HIGH_BYTE(speed_r));
+    create_write_byte(LOW_BYTE(speed_r));
+
+}
+
+void create_stop() {
+    create_write_byte(OI_DRIVE_DIRECT);
+    create_write_byte(HIGH_BYTE(0));
+    create_write_byte(LOW_BYTE(0));
+    create_write_byte(HIGH_BYTE(0));
+    create_write_byte(LOW_BYTE(0));
+}
+
+
+void create_drives_straight(const unsigned short speed, const short millimeters) {
+    double timeToGoal = (double) ((float) millimeters / (float) speed);
+
+    create_drives_direct(speed, speed);
+    msleep(timeToGoal * 1000);
+    create_stop();
+
+}
+
+void create_setup() {
+    init_list(10, 70);
+    insert_sorted(20, 80);
+    insert_sorted(30, 80);
+    insert_sorted(50, 200);
+    insert_sorted(75, 190);
+    insert_sorted(100, 250);
+    insert_sorted(150, 250);
+    insert_sorted(200, 300);
+    insert_sorted(300, 310);
+}
