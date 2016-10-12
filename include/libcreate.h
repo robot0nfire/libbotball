d8a94e89895667d08fd23f2273ba11cada0e2e1d
Nico Kratky <nico@nicokratky.me>
2016-10-12 16:35:52 +0200

    moved libcreate.h into include dir, is now an actual header file

---
+++ include/libcreate.h
@@ -0,0 +1,10 @@
+#define LOW_BYTE(x) ((x) & 0xFF)
+#define HIGH_BYTE(x) (((x) & 0xFF00) >> 8)
+
+void create_drives_direct(const short speed_l, const short speed_r);
+
+void create_stop();
+
+void create_drives_straight(const unsigned short speed, const short millimeters);
+
+void create_setup();
