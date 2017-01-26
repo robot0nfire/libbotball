7c57a9044c1fff001d34a322b018fd55bba52302
Nico Kratky <nico@nicokratky.me>
2017-01-26 11:14:27 +0000

    added libbotball files

---
+++ include/libbotball.h
@@ -0,0 +1,6 @@
+#ifndef LIBBOTBALL_H /* Include guard */
+#define LIBBOTBALL_H
+
+#include "libcreate.h"
+
+#endif /* LIBBOTBALL_H */
---
+++ libbotball.c
@@ -0,0 +1,5 @@
+#include <kipr/botball.h>
+#include "include/libbotball.h"
+#include "include/libcreate.h"
+#include "include/create_codes.h"
+#include "include/create_velocities.h"
