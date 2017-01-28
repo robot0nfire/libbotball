6e88191cbc596d9499685a1fd2f2bc99a6f41bfa
Nico Kratky <nico@nicokratky.me>
2017-01-28 21:47:08 +0100

    added inline documentation

--- create_velocities.c
+++ create_velocities.c
@@ -1,8 +1,28 @@
+/**
+* @file create_velocities.c
+* @brief This file implements a linked list to store driving data for the iRobot Create2.
+*
+* @author Nico Kratky
+*
+* @date 28/1/2017
+*/
+
 #include <stdlib.h>
 #include "include/create_velocities.h"
 
+/**
+* @brief Root Node of the linked list
+*/
 struct Node *root;
 
+
+/**
+* @brief This method initializes the linked list and stores the first data pair.
+* @author Nico Kratky
+*
+* @param distance The distance that will be driven
+* @param velocity The velocity at which the distance is reached accurately
+*/
 struct Node* init_list(int distance, int velocity) {
     struct Node *ptr = (struct Node*) malloc(sizeof(struct Node));
 
@@ -18,6 +38,13 @@ struct Node* init_list(int distance, int velocity) {
     return ptr;
 }
 
+/**
+* @brief This method inserts a data pair into the linked list.
+* @author Nico Kratky
+*
+* @param distance The distance that will be driven
+* @param velocity The velocity at which the distance is reached accurately
+*/
 struct Node* insert_sorted(int distance, int velocity) {
     if(root == NULL) {
         return (init_list(distance, velocity));
@@ -41,6 +68,12 @@ struct Node* insert_sorted(int distance, int velocity) {
     return ptr;
 }
 
+/**
+* @brief This method is used to retrieve the velocity at which the given distance is reached accurately
+* @author Nico Kratky
+*
+* @param distance The distance that will be driven+
+*/
 int get_velocity(int distance) {
     if(root == NULL) {
         return 0;
--- libbotball.c
+++ libbotball.c
@@ -1,3 +1,12 @@
+/**
+* @file libbotball.c
+* @brief Libbotball is robot0nfire's library for interacting with Botball robots.
+*
+* @author Nico Kratky
+*
+* @date 28/1/2017
+*/
+
 #include <kipr/botball.h>
 #include "include/libbotball.h"
 #include "include/libcreate.h"
