2b5b6de4001d5ab537ea5ce3bd62106b976f588d
Nico Kratky <nico@nicokratky.me>
2017-01-31 08:50:56 +0100

    added functions for interacting with the Botball servos

---
+++ include/servo.h
@@ -0,0 +1,115 @@
+/**
+* @file servo.h
+* @brief This file provides functions to interact with the Botball servos.
+*
+* @author Nico Kratky
+*
+* @date 30/1/2017
+*/
+
+#ifndef SERVO_H /* Include guard */
+#define SERVO_H
+
+#define servoMinTicks 0
+#define servoMaxTicks 2047
+
+//for seek time calculation
+#define degreesPerTick 8789
+#define timePerDegree 283333
+#define calcDivisor  100000
+
+int servoBlocked[4] = {0, 0, 0, 0};
+int servoPositions[4] = {0, 0, 0, 0};
+
+int is_blocked(int servo){
+    return servoBlocked[servo];
+}
+
+void wait_for_servo(int servo){
+    while(servoBlocked[servo]){
+        msleep(1);
+    }
+}
+
+/**
+* @brief Calculates the time in ms to wait before the whole movement of the servo is done at lowest energy level
+* http://www.servodatabase.com/servo/towerpro/sg-5010 - servo data sheet
+* @author Nico Kratky
+*
+* @param currentPosition Position in ticks at which the servo currenty is
+* @param finalPosition Position in ticks to which the servo will be moved
+*/
+void servo_get_seek_time(unsigned int currentPosition, unsigned int finalPosition);
+
+/**
+* @brief This method moves the given servo to the desired position
+* @author Nico Kratky
+*
+* @param port The port at which the servo is connected to the wallaby
+* @param position The position to which the servo should be moved
+*/
+void ssp(unsigned int port, unsigned int position);
+
+/**
+* @brief Moves a servo with a given speed and given step size "slow" to it's destination
+* @author Nico Kratky
+*
+* @param port The port at which the servo is connected to the wallaby
+* @param position The position to which the servo should be moved
+* @param stepsize How 'slow' the servo should be moved
+*/
+void ssp_stepwise(unsigned int port, unsigned int position, unsigned int stepsize, unsigned int sleep);
+
+/**
+* @brief This method moves two given servos asymmetrically to the given position
+* @author Nico Kratky
+*
+* @param port1 The port at which the first servo is connected to the wallaby
+* @param port2 The port at which the second servo is connected to the wallaby
+* @param position The position to which the servo should be moved
+*/
+void ssp_asym(unsigned int port1, unsigned int port2, unsigned int position);
+
+/**
+* @brief This method moves servo 1 to the position defined in the positions array
+* @author Nico Kratky
+*/
+void setServo0();
+
+/**
+* @brief This method moves servo 2 to the position defined in the positions array
+* @author Nico Kratky
+*/
+void setServo1();
+
+/**
+* @brief This method moves servo 3 to the position defined in the positions array
+* @author Nico Kratky
+*/
+void setServo2();
+
+/**
+* @brief This method moves servo 4 to the position defined in the positions array
+* @author Nico Kratky
+*/
+void setServo3();
+
+/**
+* @brief This method moves the given servo asynchronously to the desired position
+* @author Nico Kratky
+*
+* @param port The port at which the servo is connected to the wallaby
+* @param position The position to which the servo should be moved
+*/
+void ssp_async(unsigned int port, unsigned int position);
+
+/**
+* @brief This method moves the given servo stepwise and asynchronously to the desired position
+* @author Nico Kratky
+*
+* @param port The port at which the servo is connected to the wallaby
+* @param position The position to which the servo should be moved
+*/
+void ssp_stepwise_async(unsigned int port, unsigned int position);
+
+#endif /* SERVO_H */
---
+++ servo.c
@@ -0,0 +1,131 @@
+/**
+* @file servo.c
+* @brief This file provides functions to interact with the Botball servos.
+*
+* @author Nico Kratky
+*
+* @date 27/1/2017
+*/
+
+#include <kipr/botball.h>
+#include "include/servo.h"
+
+/**
+* Season specific parameters for moving servos asynchronously
+*/
+#define servoTickSize 10
+#define servoSleep 17
+#define servoSleepShort 10
+
+void servo_get_seek_time(unsigned int currentPosition, unsigned int finalPosition) {
+    int ticks_to_move = (currentPosition > finalPosition)? currentPosition - finalPosition : finalPosition - currentPosition;
+
+    return (int)(((ticks_to_move * degreesPerTick * timePerDegree) / calcDivisor) / calcDivisor);
+}
+
+void ssp(unsigned int port, int position) {
+    int oldPos = get_servo_position(port);
+
+    if(position > servoMaxTicks) {
+        position = servoMaxTicks;
+    } else if(position < servoMinTicks) {
+        position = servoMinTicks;
+    }
+
+    set_servo_position(servo, position);
+    msleep(servo_get_seek_time(oldPos, position));
+
+    printf("Moved servo to position: %d\n", get_servo_position(port));
+}
+
+void ssp_stepwise(unsigned int port, unsigned int position, unsigned int stepsize, unsigned int sleep) {
+    if(position > servoMaxTicks) {
+        position = servoMaxTicks;
+    } else if(position < servoMinTicks) {
+        position = servoMinTicks;
+    }
+
+    int currentPosition = get_servo_position(port);
+
+    if(position == currentPosition) { return; }
+
+    if(position > currentPosition) {
+        while(position != currentPosition) {
+            currentPosition = ((currentPosition + stepsize) > position) ? position : (currentPosition + stepsize);
+            set_servo_position(port, currentPosition);
+            msleep(sleep);
+        }
+    } else {
+        while(position != currentPosition){
+            currentPosition = ((currentPosition - stepsize) < position) ? position : (currentPosition - stepsize);
+            set_servo_position(port, currentPosition);
+            msleep(sleep);
+        }
+    }
+}
+
+void ssp_asym(unsigned int port1, unsigned int port2, unsigned int position) {
+    if(position > servoMaxTicks) {
+        position = servoMaxTicks;
+    } else if(position < servoMinTicks) {
+        position = servoMinTicks;
+    }
+
+    int pos1 = position;
+    int pos2 = servoMaxTicks - position;
+
+    wait_for_servo(port1);
+    wait_for_servo(port2);
+
+    set_servo_position(port1, pos1);
+    set_servo_position(port2, pos2);
+    msleep(300);
+
+    printf("Moved servoA to position: %d\n", get_servo_position(port1));
+    printf("Moved servoB to position: %d\n", get_servo_position(port2));
+}
+
+void setServo0(){
+    printf("Move Servo 0 to %d", servoPositions[0]);
+    ssp_stepwise(0, servoPositions[0], servoTickSize, servoSleep);
+    servoBlocked[0] = 0;
+}
+
+void setServo1(){
+    printf("Move Servo 1 to %d", servoPositions[1]);
+    ssp_stepwise(1, servoPositions[1], servoTickSize, servoSleep);
+    servoBlocked[1] = 0;
+}
+
+void setServo2(){
+    printf("Move Servo 2 to %d", servoPositions[2]);
+    ssp_stepwise(2, servoPositions[2], servoTickSize, servoSleepShort);
+    servoBlocked[2] = 0;
+}
+
+void setServo3(){
+    printf("Move Servo 3 to %d", servoPositions[3]);
+    ssp_stepwise(3, servoPositions[3], servoTickSize, servoSleep);
+    servoBlocked[3] = 0;
+}
+
+void ssp_async(unsigned int port, unsigned int position) {
+    wait_for_servo(port);
+
+    servoBlocked[port] = 1;
+    servoPositions[port] = position;
+
+    thread run;
+    if(servo == 0){
+        run = thread_create(setServo0);
+    } else if(servo == 1){
+        run = thread_create(setServo1);
+    } else if(servo == 2){
+        run = thread_create(setServo2);
+    } else if(servo == 3){
+        run = thread_create(setServo3);
+    }
+
+    thread_start(run);
+    printf("Started Servo thread\n");
+}
