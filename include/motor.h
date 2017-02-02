8358ab5785bd7fac6e202337887c54bc55f65121
Nico Kratky <nico@nicokratky.me>
2017-02-02 14:04:25 +0100

    added functions for interacting with the Botball motors

---
+++ include/motor.h
@@ -0,0 +1,72 @@
+/**
+* @file motor.h
+* @brief This file provides functions to interact with the Botball motors.
+*
+* @author Nico Kratky
+*
+* @date 2/2/2017
+*/
+
+#ifndef SERVO_H /* Include guard */
+#define SERVO_H
+
+/**
+* @brief Port to which the left driving motor is connected
+*/
+#define LEFT_MOTOR_DRIVE 0
+/**
+* @brief Port to which the right driving motor is connected
+*/
+#define RIGHT_MOTOR_DRIVE 3
+
+/**
+* @brief Port to which the left tophat is connected
+*/
+#define TOPHAT_LEFT 0
+/**
+* @brief Port to which the left tophat is connected
+*/
+#define TOPHAT_RIGHT 1
+
+/**
+* @brief Threshold for following a line
+*/
+#define THRESHOLD 0
+
+/**
+* @brief This method lets the robot drive with two given velocities
+* @author Nico Kratky
+*
+* @param velocity_left Velocity at which the left motor will be moved
+* @param velocity_right Velocity at which the right motor will be moved
+*/
+void drive_direct(int velocity_left, int velocity_right);
+
+/**
+* @brief This method lets the robot drive straight for a given duration
+* @author Nico Kratky
+*
+* @param velocity Velocity at which the motors will be moved
+* @param ms How long the robot will drive
+*/
+void drive_straight(int velocity, unsigned int ms);
+
+/**
+* @brief This method lets the robot drive with two given velocities for a given duration
+* @author Nico Kratky
+*
+* @param velocity_left Velocity at which the left motor will be moved
+* @param velocity_right Velocity at which the right motor will be moved
+* @param ms How long the robot will drive
+*/
+void drive(int velocity_left, int velocity_right, unsigned int ms);
+
+/**
+* @brief This method lets the robot follow a line for a given duration
+* @author Nico Kratky
+*
+* @param ms How lon the robot will follow a line
+*/
+void follow_line(unsigned int ms);
+
+#endif /* SERVO_H */
---
+++ motor.c
@@ -0,0 +1,43 @@
+/**
+* @file motor.c
+* @brief This file provides functions to interact with the Botball motors.
+*
+* @author Nico Kratky
+*
+* @date 2/2/2017
+*/
+
+void drive_direct(int velocity_left, int velocity_right) {
+    mav(LEFT_MOTOR_DRIVE, velocity_left);
+    mav(RIGHT_MOTOR_DRIVE, velocity_right);
+}
+
+void drive_straight(int velocity, unsigned int ms) {
+    mav(LEFT_MOTOR_DRIVE, velocity);
+    mav(RIGHT_MOTOR_DRIVE, -velocity);
+
+    msleep(ms);
+
+    freeze(LEFT_MOTOR_DRIVE);
+    freeze(RIGHT_MOTOR_DRIVE);
+}
+
+void drive(int velocity_left, int velocity_right, unsigned int ms) {
+    drive_direct(velocity_left, velocity_right);
+    msleep(ms);
+    freeze(LEFT_MOTOR_DRIVE);
+    freeze(RIGHT_MOTOR_DRIVE);
+}
+
+void follow_line(unsigned int ms) {
+    float end_time = seconds() + (ms / 1000);
+
+    while (seconds() < end_time) {
+        if ((analog(TOPHAT_RIGHT) < THRESHOLD) && (analog(TOPHAT_LEFT) > THRESHOLD))
+            drive(500, 1500, 10);
+        else if ((analog(TOPHAT_LEFT) < THRESHOLD) && (analog(TOPHAT_RIGHT) > THRESHOLD))
+            drive(1500, 500, 10);
+        else
+            drive(1500, 1500, 10);
+    }
+}
