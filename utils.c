c292eb61db46d29da2a416cd7cfcd7fdd06670d6
Nico Kratky <nico@nicokratky.me>
2017-02-15 17:33:28 +0100

    uppercased max 'cause im dumb

--- utils.c
+++ utils.c
@@ -5,5 +5,5 @@
 float float_close(float a, float b, float abs_tol) {
     float rel_tol = 0.00001;
 
-    if(abs(a - b) <= max(rel_tol * MAX(abs(a), abs(b)), abs_tol));
+    if(abs(a - b) <= MAX(rel_tol * MAX(abs(a), abs(b)), abs_tol));
 }
