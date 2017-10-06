2a885b7b0df3bf4079c44e62bf36bd9b0e86942f
Nico Kratky <nico@nicokratky.me>
2017-10-06 13:33:20 +0200

    sleep for positive amount of ms

--- create.c
+++ create.c
@@ -85,7 +85,7 @@ void create_spins_counterclockwise(const short velocity) {
 
 void create_spins_degrees(const short velocity, const short angle) {
     double w = ((double) velocity) /  ((double) RADIUS);
-    double timeToGoal = degToRad(angle) / w;
+    double timeToGoal = fabs(degToRad(angle) / w);
 
     printf("v: %d\n", velocity);
     printf("r: %d\n", RADIUS);
